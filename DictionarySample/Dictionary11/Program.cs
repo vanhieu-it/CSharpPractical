3ae5d453b43ee0d4e003451c5aea9717990a7a9b
Văn Hiếu <vanhieu99it@gmail.com>
2024-05-16 13:12:25 +0700

    update

---
+++ DictionarySample/Dictionary11/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, List<HashSet<Tuple<string, int>>>> complexData = new Dictionary<string, List<HashSet<Tuple<string, int>>>>();
+
+        complexData["Group1"] = new List<HashSet<Tuple<string, int>>>
+        {
+            new HashSet<Tuple<string, int>>
+            {
+                new Tuple<string, int>("Item1", 10),
+                new Tuple<string, int>("Item2", 20)
+            },
+            new HashSet<Tuple<string, int>>
+            {
+                new Tuple<string, int>("Item3", 30),
+                new Tuple<string, int>("Item4", 40)
+            }
+        };
+
+        complexData["Group2"] = new List<HashSet<Tuple<string, int>>>
+        {
+            new HashSet<Tuple<string, int>>
+            {
+                new Tuple<string, int>("Item5", 50),
+                new Tuple<string, int>("Item6", 60)
+            }
+        };
+
+        foreach (var group in complexData)
+        {
+            Console.WriteLine($"Group: {group.Key}");
+            foreach (var set in group.Value)
+            {
+                foreach (var tuple in set)
+                {
+                    Console.WriteLine($"  Item: {tuple.Item1}, Value: {tuple.Item2}");
+                }
+            }
+        }
+    }
+}
---
+++ DictionarySample/Dictionary12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Salary { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<int, Employee> employees = new Dictionary<int, Employee>
+        {
+            { 1, new Employee { Id = 1, Name = "John Doe", Salary = 50000 } },
+            { 2, new Employee { Id = 2, Name = "Jane Smith", Salary = 60000 } }
+        };
+
+        // Serialization
+        string json = JsonConvert.SerializeObject(employees, Formatting.Indented);
+        Console.WriteLine("Serialized JSON:");
+        Console.WriteLine(json);
+
+        // Deserialization
+        var deserializedEmployees = JsonConvert.DeserializeObject<Dictionary<int, Employee>>(json);
+        Console.WriteLine("\nDeserialized Dictionary:");
+        foreach (var employee in deserializedEmployees)
+        {
+            Console.WriteLine($"Id: {employee.Key}, Name: {employee.Value.Name}, Salary: {employee.Value.Salary}");
+        }
+    }
+}
