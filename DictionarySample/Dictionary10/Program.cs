4ecc06e9a9e1f246a0b3745a14202e79d2116cc4
Văn Hiếu <vanhieu99it@gmail.com>
2024-05-16 11:20:14 +0700

    add new 10

---
+++ DictionarySample/Dictionary10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        ConcurrentDictionary<int, string> concurrentDictionary = new ConcurrentDictionary<int, string>();
+        // Môi trường đa luồng
+        Parallel.For(0, 10000, i =>
+        {
+            concurrentDictionary.TryAdd(i, $"Value {i}");
+        });
+
+        foreach (var item in concurrentDictionary)
+        {
+            Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+        }
+    }
+}
