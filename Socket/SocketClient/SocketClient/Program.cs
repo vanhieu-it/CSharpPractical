59407d0a97848c3e5f2c26cf34d05816b0836d8d
Văn Hiếu <vanhieu99it@gmail.com>
2024-05-21 10:36:25 +0700

    Socket

---
+++ Socket/SocketClient/SocketClient/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Define the port and IP address for the client
+                int port = 8080;
+                string server = "127.0.0.1";
+
+                // Create a TCP/IP socket
+                TcpClient client = new TcpClient(server, port);
+
+                // Translate the passed message into ASCII and store it as a byte array.
+                Console.Write("Enter the message to send: ");
+                string message = Console.ReadLine();
+                byte[] data = Encoding.ASCII.GetBytes(message);
+
+                // Get a client stream for reading and writing.
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer.
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine($"Sent: {message}");
+
+                // Buffer to store the response bytes.
+                data = new byte[1024];
+
+                // String to store the response ASCII representation.
+                string responseData = string.Empty;
+
+                // Read the first batch of the TcpServer response bytes.
+                int bytes = stream.Read(data, 0, data.Length);
+                responseData = Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine($"Received: {responseData}");
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine($"ArgumentNullException: {e}");
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine($"SocketException: {e}");
+            }
+
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+        }
+    }
+}
---
+++ Socket/SocketServer/SocketServer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define the port and IP address for the server
+            int port = 8080;
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+            // Create a TCP/IP socket
+            TcpListener server = new TcpListener(localAddr, port);
+
+            // Start listening for client requests
+            server.Start();
+            Console.WriteLine("Server started...");
+
+            while (true)
+            {
+                Console.WriteLine("Waiting for a connection...");
+
+                // Perform a blocking call to accept requests.
+                // You could also use server.AcceptTcpClientAsync() for an asynchronous version.
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Connected!");
+
+                // Get a stream object for reading and writing
+                NetworkStream stream = client.GetStream();
+
+                byte[] bytes = new byte[1024];
+                int i;
+
+                // Loop to receive all the data sent by the client.
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    // Translate data bytes to a ASCII string.
+                    string data = Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine($"Received: {data}");
+
+                    // Process the data sent by the client.
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    // Send back a response.
+                    stream.Write(msg, 0, msg.Length);
+                    Console.WriteLine($"Sent: {data}");
+                }
+
+                // Shutdown and end connection
+                client.Close();
+            }
+        }
+    }
+}
