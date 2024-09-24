d0bcc6a17df2f0620ec7c53aafb1859dcf27f1e3
Văn Hiếu <vanhieu99it@gmail.com>
2024-09-24 10:59:37 +0700

    WebSocketGRPC

---
+++ WebSocketGRPCDemo/WebSocketGRPCDemo/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Net.WebSockets;
+using WebSocketGRPCDemo.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseWebSockets(new WebSocketOptions
+{
+    KeepAliveInterval = TimeSpan.FromSeconds(2)//Thời gian giữ kết nối Web Socket
+});
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapGrpcService<GreeterService>();
+
+app.MapControllers();
+
+app.MapGet("/", () => "Use a gRPC client to communicate with the gRPC server.");
+
+app.Map("/ws", async context =>
+{
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+        await EchoWebSocket(context, webSocket);
+    }
+    else
+    {
+        context.Response.StatusCode = 400;
+    }
+});
+
+app.Run();
+
+
+// Hàm xử lý WebSocket
+async Task EchoWebSocket(HttpContext context, WebSocket webSocket)
+{
+    var buffer = new byte[1024 * 4];
+    WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+    while (!result.CloseStatus.HasValue)
+    {
+        // Echo lại dữ liệu mà WebSocket client đã gửi
+        await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+        result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+    }
+
+    await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+}
